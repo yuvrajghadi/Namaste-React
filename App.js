e3edc358a918fb5164448fd5a7c9eeb3fef4ef4a
yuvrajghadi <114417851+yuvrajghadi@users.noreply.github.com>
2025-07-17 09:53:24 +0530

    make changes in code

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child", key: "child1" }, [
-    React.createElement("h1", { key: "h1-1" }, "This is React"),
-    React.createElement("h2", { key: "h2-1" }, "By Yuvraj"),
-  ]),
-  React.createElement("div", { id: "child2", key: "child2" }, [
-    React.createElement("h1", { key: "h1-2" }, "4567"),
-    React.createElement("h2", { key: "h2-2" }, "I'm h2"),
-  ])
-]);
+const parent = React.createElement("h1", { id: "heading" }, "Namaste React");
 
+
+const Title=() => (
+  <div>
+    <h1 id="heading">Component Composition🚀</h1>
+  </div>
+)
+
+// component composition
+const Headingcomponent=() => (
+  <div>
+    <Title />
+    <h1 id="heading">Namaste React using JSX🚀</h1>
+  </div>
+)
+
+
+
+const jsxHeaading = <h1 id="heading">Namaste React using JSX🚀</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<Headingcomponent />);
